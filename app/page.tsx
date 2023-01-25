ada87c781af761fb262033eaf67ab6e7c6009113
oscareng <orolf001@ucr.edu>
2023-01-25 05:25:40 -0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import { Layout, Text, Page } from "@vercel/examples-ui";
+import { Chat } from "../components/Chat";
+
+function Home() {
+  return (
+    <Page className="flex flex-col gap-12">
+      <section className="flex flex-col gap-6">
+        <Text variant="h1">OpenAI GPT-3</Text>
+      </section>
+
+      <section className="flex flex-col gap-3">
+        <div className="lg:w-2/3">
+          <Chat />
+        </div>
+      </section>
+    </Page>
+  );
+}
+
+Home.Layout = Layout;
+
+export default Home;
